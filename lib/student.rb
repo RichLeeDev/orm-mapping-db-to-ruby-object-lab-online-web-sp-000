0110d20c38e35bc6375e7a767779e66b10f48e6d
Richard Lee <richlee81192@gmail.com>
2020-01-04 23:59:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,6 +97,18 @@ class Student
     end.first
   end
   
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT x
+    SQL
+    
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
   
   
   
